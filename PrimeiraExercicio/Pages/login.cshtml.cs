c86d04c336aab56738cabf9ecb2e25e88c9da282
Leonardo Matumona <leonardo.matumona@outlook.com>
2020-10-27 15:43:04 -0300

    Add project files.

---
+++ PrimeiraExercicio/Pages/login.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PrimeiraExercicio.Pages
+{
+    public class loginModel : PageModel
+    {
+        private readonly ILogger<loginModel> _logger;
+
+        public loginModel(ILogger<loginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
